4d5066496cc9d8062a005d35dcf8bcb5467e8deb
lixianfa <lixianfa.official@gmail.com>
2019-04-15 20:12:03 +0800

    get hardware and software info

--- docs/script/hardware_software_conf.sh
+++ docs/script/hardware_software_conf.sh
@@ -1,29 +1,61 @@
 #!/bin/bash
 HOME_DIR=$(cd $(dirname $0);pwd)
-hardware_software_conf=$HOME_DIR"/hardware_software_conf.txt"
+hardware_software_conf=$HOME_DIR"/out_hardware_software_conf.txt"
 
 echo "export system hardware and software configuration"
 
-if[ -e "$LOG_DETAIL" ];then
-    rm $LOG_DETAIL
+if [ -e "$LOG_DETAIL" ];then
+    echo "" > $hardware_software_conf
+fi
 
 echo "lscpu" | tee -a $hardware_software_conf
 lscpu >> $hardware_software_conf
+wait
+printf "\n\n****************\n" | tee -a $hardware_software_conf
+
+
+echo "lsblk" | tee -a $hardware_software_conf
+lsblk >> $hardware_software_conf
+wait
+printf "\n\n****************\n" | tee -a $hardware_software_conf
+
+
+echo "df" | tee -a $hardware_software_conf
+df -h >> $hardware_software_conf
+wait
+printf "\n\n****************\n" | tee -a $hardware_software_conf
+
+echo "smartctl" | tee -a $hardware_software_conf
+for disk in {a..l};do
+    if [ -b "/dev/sd${disk}" ];then
+        echo "/dev/sd${disk}"
+        smartctl -a "/dev/sd${disk}" >> $hardware_software_conf
+        wait
+    fi
+done
+printf "\n\n****************\n" | tee -a $hardware_software_conf
+
+echo "ip a" | tee -a $hardware_software_conf
+ip a >> $hardware_software_conf
+wait
+printf "\n\n****************\n" | tee -a $hardware_software_conf
+
+echo "lspci" | tee -a $hardware_software_conf
+lspci -tv >> $hardware_software_conf
+printf "\n\n****************\n" | tee -a $hardware_software_conf
 
 echo "dmidecode" | tee -a $hardware_software_conf
-var type=(bios
-system
-baseboard
-chassis
-processor
-memory
-cache
-connector
-slot)
-
-for t in $type ; do
+typ=("bios" "system" "baseboard" "chassis" "processor" "memory" "cache" "connector" "slot")
+for t in "${typ[@]}" ; do
     dmidecode -t $t >> $hardware_software_conf
+    wait
 done
+printf "\n\n****************\n" | tee -a $hardware_software_conf
 
+echo "cat /proc/cupinfo" | tee -a $hardware_software_conf
+cat /proc/cpuinfo >> $hardware_software_conf
+printf "\n\n****************\n" | tee -a $hardware_software_conf
 
-
+echo "cat /proc/meminfo" | tee -a $hardware_software_conf
+cat /proc/meminfo >> $hardware_software_conf
+printf "\n\n****************\n" | tee -a $hardware_software_conf
