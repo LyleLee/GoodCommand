b4ddba67d272f5e90c5abc875d779b7ace1ef994
lixianfa <lixianfa.official@gmail.com>
2019-05-08 15:55:23 +0800

    fio: script to run fio on both arm and x86

---
+++ docs/script/descriptor_read_register.sh
@@ -0,0 +1,113 @@
+#~/bin/bash
+HOME_DIR=$(cd $(dirname $0);pwd)
+sysrq_file=$HOME_DIR"/sysrq_value.txt"
+register_file=$HOME_DIR"/register_value.txt"
+
+if [ -e "$sysrq_file" ];then
+    echo "" > $sysrq_file
+fi
+
+if [ -e "$register_file" ];then
+    echo "" >$register_file
+fi
+
+dmesg -C 
+
+echo "0" > /proc/sys/kernel/sysrq
+echo "0" > /proc/sys/kernel/softlockup_panic 
+
+
+for (( ; ; ));do
+    
+    1>>$register_file
+    echo "Dumps tasks that are in uninterruptable (blocked) state." |tee -a $sysrq_file
+    echo w > /proc/sysrq-trigger
+    dmesg -T >> $sysrq_file
+    wait
+    echo -e "\n\n-------------------------------------------------------\n" >> $sysrq_file
+    dmesg -C
+    
+    
+    
+    echo "  #chip0	    #chip0                  $(                    ) "    >> $register_file
+    echo "  #L3D	    #L3D                    $(                    ) "    >> $register_file
+    echo "  devmem 0x90141030 32  	            $(devmem 0x90141030 32) "    >> $register_file
+    echo "  devmem 0x90151030 32 	            $(devmem 0x90151030 32) "    >> $register_file
+    echo "  devmem 0x90161030 32 	            $(devmem 0x90161030 32) "    >> $register_file
+    echo "  devmem 0x90171030 32	            $(devmem 0x90171030 32) "    >> $register_file
+    echo "  #L3T	    #L3T                    $(                    ) "    >> $register_file
+    echo "  devmem 0x90181030 32	            $(devmem 0x90181030 32) "    >> $register_file
+    echo "  devmem 0x90191030 32	            $(devmem 0x90191030 32) "    >> $register_file
+    echo "  devmem 0x901a1030 32	            $(devmem 0x901a1030 32) "    >> $register_file
+    echo "  devmem 0x901b1030 32	            $(devmem 0x901b1030 32) "    >> $register_file
+    echo "  devmem 0x901c1030 32	            $(devmem 0x901c1030 32) "    >> $register_file
+    echo "  devmem 0x901d1030 32	            $(devmem 0x901d1030 32) "    >> $register_file
+    echo "  devmem 0x901e1030 32	            $(devmem 0x901e1030 32) "    >> $register_file
+    echo "  devmem 0x901f1030 32	            $(devmem 0x901f1030 32) "    >> $register_file
+    echo "  #HHA	    #HHA                    $(                    ) "    >> $register_file
+    echo "  devmem 0x90121000 32	            $(devmem 0x90121000 32) "    >> $register_file
+    echo "  devmem 0x90121000 32	            $(devmem 0x90121000 32) "    >> $register_file
+    echo "  #MN	    #MN                         $(                    ) "    >> $register_file
+    echo "  devmem 0x90331030 32	            $(devmem 0x90331030 32) "    >> $register_file
+    echo "  #L3D	    #L3D                    $(                    ) "    >> $register_file
+    echo "  devmem 0x98141030 32	            $(devmem 0x98141030 32) "    >> $register_file
+    echo "  devmem 0x98151030 32	            $(devmem 0x98151030 32) "    >> $register_file
+    echo "  devmem 0x98161030 32	            $(devmem 0x98161030 32) "    >> $register_file
+    echo "  devmem 0x98171030 32	            $(devmem 0x98171030 32) "    >> $register_file
+    echo "  #L3T	    #L3T                    $(                    ) "    >> $register_file
+    echo "  devmem 0x98181030 32	            $(devmem 0x98181030 32) "    >> $register_file
+    echo "  devmem 0x98191030 32	            $(devmem 0x98191030 32) "    >> $register_file
+    echo "  devmem 0x981a1030 32	            $(devmem 0x981a1030 32) "    >> $register_file
+    echo "  devmem 0x981b1030 32	            $(devmem 0x981b1030 32) "    >> $register_file
+    echo "  devmem 0x981c1030 32	            $(devmem 0x981c1030 32) "    >> $register_file
+    echo "  devmem 0x981d1030 32	            $(devmem 0x981d1030 32) "    >> $register_file
+    echo "  devmem 0x981e1030 32	            $(devmem 0x981e1030 32) "    >> $register_file
+    echo "  devmem 0x981f1030 32	            $(devmem 0x981f1030 32) "    >> $register_file
+    echo "  #HHA	    #HHA                    $(                    ) "    >> $register_file
+    echo "  devmem 0x98121000 32	            $(devmem 0x98121000 32) "    >> $register_file
+    echo "  devmem 0x98121000 32	            $(devmem 0x98121000 32) "    >> $register_file
+    echo "  #MN	    #MN                         $(                    ) "    >> $register_file
+    echo "  devmem 0x98331030 32	            $(devmem 0x98331030 32) "    >> $register_file
+    echo "  #chip1	    #chip1                                        "      >> $register_file
+    echo "  #L3D	    #L3D                                          "      >> $register_file
+    echo "  devmem 0x400090141030 32	   $(devmem 0x400090141030 32)  "    >> $register_file
+    echo "  devmem 0x400090151030 32	   $(devmem 0x400090151030 32)  "    >> $register_file
+    echo "  devmem 0x400090161030 32	   $(devmem 0x400090161030 32)  "    >> $register_file
+    echo "  devmem 0x400090171030 32	   $(devmem 0x400090171030 32)  "    >> $register_file
+    echo "  #L3T	    #L3T               $(                        )  "    >> $register_file
+    echo "  devmem 0x400090181030 32	   $(devmem 0x400090181030 32)  "    >> $register_file
+    echo "  devmem 0x400090191030 32	   $(devmem 0x400090191030 32)  "    >> $register_file
+    echo "  devmem 0x4000901a1030 32	   $(devmem 0x4000901a1030 32)  "    >> $register_file
+    echo "  devmem 0x4000901b1030 32	   $(devmem 0x4000901b1030 32)  "    >> $register_file
+    echo "  devmem 0x4000901c1030 32	   $(devmem 0x4000901c1030 32)  "    >> $register_file
+    echo "  devmem 0x4000901d1030 32	   $(devmem 0x4000901d1030 32)  "    >> $register_file
+    echo "  devmem 0x4000901e1030 32	   $(devmem 0x4000901e1030 32)  "    >> $register_file
+    echo "  devmem 0x4000901f1030 32	   $(devmem 0x4000901f1030 32)  "    >> $register_file
+    echo "  #HHA	    #HHA               $(                        )  "    >> $register_file
+    echo "  devmem 0x400090121000 32	   $(devmem 0x400090121000 32)  "    >> $register_file
+    echo "  devmem 0x400090121000 32	   $(devmem 0x400090121000 32)  "    >> $register_file
+    echo "  #MN	    #MN                    $(                        )  "    >> $register_file
+    echo "  devmem 0x400090331030 32	   $(devmem 0x400090331030 32)  "    >> $register_file
+    echo "  #L3D	    #L3D               $(                        )  "    >> $register_file
+    echo "  devmem 0x400098141030 32	   $(devmem 0x400098141030 32)  "    >> $register_file
+    echo "  devmem 0x400098151030 32	   $(devmem 0x400098151030 32)  "    >> $register_file
+    echo "  devmem 0x400098161030 32	   $(devmem 0x400098161030 32)  "    >> $register_file
+    echo "  devmem 0x400098171030 32	   $(devmem 0x400098171030 32)  "    >> $register_file
+    echo "  #L3T	    #L3T               $(                        )  "    >> $register_file
+    echo "  devmem 0x400098181030 32	   $(devmem 0x400098181030 32)  "    >> $register_file
+    echo "  devmem 0x400098191030 32	   $(devmem 0x400098191030 32)  "    >> $register_file
+    echo "  devmem 0x4000981a1030 32	   $(devmem 0x4000981a1030 32)  "    >> $register_file
+    echo "  devmem 0x4000981b1030 32	   $(devmem 0x4000981b1030 32)  "    >> $register_file
+    echo "  devmem 0x4000981c1030 32	   $(devmem 0x4000981c1030 32)  "    >> $register_file
+    echo "  devmem 0x4000981d1030 32	   $(devmem 0x4000981d1030 32)  "    >> $register_file
+    echo "  devmem 0x4000981e1030 32	   $(devmem 0x4000981e1030 32)  "    >> $register_file
+    echo "  devmem 0x4000981f1030 32	   $(devmem 0x4000981f1030 32)  "    >> $register_file
+    echo "  #HHA	    #HHA               $(                        )  "    >> $register_file
+    echo "  devmem 0x400098121000 32	   $(devmem 0x400098121000 32)  "    >> $register_file
+    echo "  devmem 0x400098121000 32	   $(devmem 0x400098121000 32)  "    >> $register_file
+    echo "  #MN	    #MN                    $(                        )  "    >> $register_file
+    echo "  devmem 0x400098331030 32	   $(devmem 0x400098331030 32)  "    >> $register_file
+    echo "  ----------------------------------------------------------- "    >> $register_file 
+    
+    sleep 5s
+done
--- docs/script/io_all.sh
+++ docs/script/io_all.sh
@@ -2,21 +2,46 @@
 
 #config here you disk you want to run fio
 
-DISK="/dev/sdb"
-SDX="${DISK##*/}" #get sdb. delete charactor from left to right until meet /
+#config disk to run fio
+DISK="/dev/sdd"
 
-HOME_DIR=$(cd "$(dirname "$0")";pwd)
-LOG_DETAIL="${HOME_DIR}/$(uname -m)_${SDX}_fio_detail.txt"
-LOG_SIMPLE="${HOME_DIR}/$(uname -m)_${SDX}_fio_SIMPLE.txt"
+#configure runtime
+RUN_TIME=60
 
-
-
-if [ -e $LOG_DETAIL ]; then
-	echo "" > $LOG_DETAIL
+#查询硬盘是否是否存在
+if [ ! -e "${DISK}" ]; then
+	echo "${DISK} disk not exists, script exit"
+	exit
 fi
 
-if [ -e $LOG_SIMPLE ]; then
-	echo "" > $LOG_SIMPLE
+#查询硬盘是否挂载，挂载则不执行fio
+if [ "$(grep "${DISK}" /proc/mounts)" ] ; then
+	echo -e "your device:$DISK is mounted: \n$(grep "${DISK}" /proc/mounts) \nscript exit!"
+	exit
+fi
+
+echo "Disk you want to test is $DISK each runtime $RUN_TIME"
+
+SDX="${DISK##*/}" #get sdc. delete charactor from left to right until meet /
+CPU_NAME="$(sudo dmidecode -s processor-version | uniq)"
+
+HOME_DIR=$(cd "$(dirname "$0")" || exit ;pwd)
+
+#设置CPU型号的命名
+if [ "$CPU_NAME" ]; then
+	LOG_DETAIL="${HOME_DIR}/${CPU_NAME}_$(uname -m)_${SDX}_fio_detail.txt"
+	LOG_SIMPLE="${HOME_DIR}/${CPU_NAME}_$(uname -m)_${SDX}_fio_simple.txt"
+else
+	LOG_DETAIL="${HOME_DIR}/$(uname -m)_${SDX}_fio_detail.txt"
+	LOG_SIMPLE="${HOME_DIR}/$(uname -m)_${SDX}_fio_simple.txt"
+fi
+
+if [ -e "$LOG_DETAIL" ]; then
+	echo "" > "$LOG_DETAIL"
+fi
+
+if [ -e "$LOG_SIMPLE" ]; then
+	echo "" > "$LOG_SIMPLE"
 fi
 
 bs=(4k 8k 1m 4m)
@@ -26,41 +51,46 @@ iodepth=(1 8 16 32 64 128 256)
 
 count=0
 
-printf "number\t  block_size\t numjobs\t iodepth\t iops\t bandwith\t lat\t cpu_user\t cpu_sys\n" >> $LOG_SIMPLE
+printf "number  block_size read_write  numjobs   iodepth        " | tee -a "${LOG_SIMPLE}"
+if [ "$(uname -m)" = "aarch64" ]; then
+	 printf "numactl configuration if there are any    " | tee -a "${LOG_SIMPLE}"
+fi
+printf "IOPS     bandwith     latency    cpu_user  cpu_sys\n"| tee -a "${LOG_SIMPLE}"
 
-for b in ${bs[@]}; do
-    for ro in ${rw[@]}; do
-		for n in ${numjob[@]}; do
-			for d in ${iodepth[@]}; do
-                printf "%d\t %s\t %-10s\t %s\t %d\t " $count $b $ro $n $d | tee -a $LOG_SIMPLE -a $LOG_DETAIL #输出到屏幕同时到文件
-				echo " -----------------------------------------------------" >> $LOG_DETAIL
+
+for b in "${bs[@]}"; do
+    for ro in "${rw[@]}"; do
+		for n in "${numjob[@]}"; do
+			for d in "${iodepth[@]}"; do
+                printf "%d\t %s\t %-10s\t %s\t %d\t " "${count}" "${b}" "${ro}" "${n}" "${d}" | tee -a "$LOG_SIMPLE" -a "$LOG_DETAIL" #输出到屏幕同时到文件
+				echo " -----------------------------------------------------" >> "$LOG_DETAIL"
                 
-                if [ $(uname -m) = "aarch64" ]; then
-                    numa="numactl -C 0-$(( $n-1 )) -m 0 "
-                    printf "%s\t" $numa | tee -a $LOG_SIMPLE
+                if [ "$(uname -m)" = "aarch64" ]; then
+                    numa="numactl -C 0-"$(( $n-1 ))" -m 0 "
+                    printf "%s\t" "$numa" | tee -a "$LOG_SIMPLE"
                 else
                     numa=""
                 fi
-                result=$(${numa} fio --ramp_time=5 --runtime=60 --ioengine=libaio --filename=$DISK --name="$count-$b-$ro-$n-$d" --bs=$b \
-                        --rw=$ro \
-                        --numjobs=$n \
-                        --iodepth=$d \
+                result=$(${numa} fio --ramp_time=5 --runtime="$RUN_TIME" --ioengine=libaio --filename=$DISK --name="$count-$b-$ro-$n-$d" --bs="$b" \
+                        --rw="$ro" \
+                        --numjobs="$n" \
+                        --iodepth="$d" \
                         --direct=1 \
                         --group_report)
                 while [ "$(pgrep fio)" ]; do
                     echo "need to sleep a while when fio is running"
                     sleep 5
                 done
-                echo "$result" >> $LOG_DETAIL
+                echo "$result" >> "$LOG_DETAIL"
                 iops_bandwith=$(echo "$result" | grep "IOPS=")
                 iops=$(echo "$iops_bandwith" | awk '{print $2}'|awk -F '[=,]' '{print $2}')
                 bandwith=$(echo "$iops_bandwith" | awk -F '[()]' '{print $2}')
                 lat=$(echo "$result" | grep "\ lat.*avg"| awk -F, '{print $3}'|awk -F= '{print $2}')
                 cpu_user=$(echo "$result" | grep cpu |awk -F '[:,=]' '{print $3}')
                 cpu_sys=$(echo "$result" | grep cpu |awk -F '[:,=]' '{print $5}')
-                printf "%s\t %s\t %s\t %s\t %s\t\n" $iops $bandwith $lat $cpu_user $cpu_sys | tee -a $LOG_SIMPLE
-                echo "" >> $LOG_DETAIL
-                (( count++ ))  
+                printf "%s\t %s\t %-10s\t %s\t %s\t\n" "$iops" "$bandwith" "$lat" "$cpu_user" "$cpu_sys" | tee -a "$LOG_SIMPLE"
+                echo "" >> "$LOG_DETAIL"
+                (( count++ ))
 			done
 		done
 	done
