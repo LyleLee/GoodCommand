856733ff91e7865e91bda2a2396ade6d3541ac10
lixianfa <lixianfa.official@gmail.com>
2019-09-23 19:32:07 +0800

    leetcode: 151-reverse-words-in-a-string.c

--- docs/src/leetcode/151-reverse-words-in-a-string.c
+++ docs/src/leetcode/151-reverse-words-in-a-string.c
@@ -13,38 +13,51 @@ void px(char *s, int len)
 char * reverseWords(char * s){
 	int len = strlen(s);
 	char * r = (char *) malloc(len+1);
+	memset(r, 0, (len+1));
 	if(len==0)return r;
 	int i=len-1;
 	int j=0;
 	int word_begin = len-1;
 	int word_end = len-1;
-	while(i>0)
+	while(i>=0)
 	{
 		while(i != -1 && s[i] == ' ')i--;
+		if(i == -1) return r;
 		word_end = i;
-		while(i!=-1 && s[i] != ' ')i--;
-		word_begin = i+1;
-		//printf("begin:%d, end:%d,j:%d\n",word_begin,word_end,j);
+		while(i>=0 && s[i] != ' ')i--;
+		if(i==-1)word_begin = 0;
+		else if(s[i]==' ')word_begin=i+1;
+		else printf("error...");
 		//px(s,len);
+		//printf("begin:%d, end:%d,j:%d\n",word_begin,word_end,j);
+		if( j!= 0)r[j++]=' ';
 		strncpy((r+j), s+word_begin, word_end-word_begin+1);
+		i--;
 		j = j+word_end-word_begin+1;
-		r[j++]=' ';
 		//px(r, len);
 	}
-	r[j-1]=0;
-	for(;j<=len;j++)r[j]='\0';
 
 	return r;
 }
 
 int main()
 {
-	char *s1 = "the sky is blue";
-	char *s2 = "  hello world!  ";
-	char *s3 = "";
-	char *r= NULL;
-	r=reverseWords(s1); px(r,strlen(s1));free(r);
-	r=reverseWords(s2); px(r,strlen(s2));free(r);
-	r=reverseWords(s3); px(r,strlen(s3));free(r);
+	char * ca[] = {" a", "a", "the sky is blue", "  hello world!  ", "", " "};
+	int i =0;
+	char *r = NULL;
+	for(i=0; i < 5;i ++)
+	{
+		printf("case:%d input:%s\n",i,ca[i]);
+		r=reverseWords(ca[i]); px(r,strlen(ca[i]));free(r);
+		printf("\n");
+	}
+
+	//char *s6 = " a";
+	//char *s5 = "a";
+	//char *s1 = "the sky is blue";
+	//char *s2 = "  hello world!  ";
+	//char *s3 = "";
+	//char *s4 = " "; 
+	//char *r= NULL;
 	return 0;
 }
