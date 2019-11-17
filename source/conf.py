4c43e68fd8d88f4bbd1b859f3ac9faf76a927b0b
lixianfa <lixianfa.official@gmail.com>
2019-11-18 02:13:33 +0800

    sphinx: add favicon.ico

--- source/conf.py
+++ source/conf.py
@@ -56,7 +56,8 @@ exclude_patterns = []
 #
 #html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
-html_style = 'css/my_theme.css' 
+html_style = 'css/my_theme.css'
+html_favicon = 'favicon.ico'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
